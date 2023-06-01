3ef2f00a449957fa942246ead22ca4d6e40c1c12
Jasmine McKinnon <58950226+jas-mckin@users.noreply.github.com>
2023-06-01 13:49:15 +0100

    update conf.py - tabs testing

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,8 +35,8 @@ html_theme = 'sphinx_rtd_theme'
 epub_show_urls = 'footnote'
 
 # - add logo to top of site
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+# html_theme = 'sphinx_rtd_theme'
+# html_static_path = ['_static']
 # html_logo = 'images/ENA_Logo_tagline.png'
 html_theme_options = {
     'logo_only': True,
