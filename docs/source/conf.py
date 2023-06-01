ebde372f864849dd96cd95e0e2c6fc996388b0ed
Jasmine McKinnon <58950226+jas-mckin@users.noreply.github.com>
2023-06-01 13:49:39 +0100

    update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,7 @@ epub_show_urls = 'footnote'
 # html_theme = 'sphinx_rtd_theme'
 # html_static_path = ['_static']
 # html_logo = 'images/ENA_Logo_tagline.png'
-html_theme_options = {
+# -html_theme_options = {
     'logo_only': True,
     'display_version': False,
 }
