1de73cd9855076aa42dcbc1d32f3ea8d381cc150
Jasmine McKinnon <58950226+jas-mckin@users.noreply.github.com>
2023-06-02 13:27:07 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,8 +40,8 @@ epub_show_urls = 'footnote'
 
 # -- Project information
 project = 'ena-pathogen-docs'
-copyright = '2023, Licensed under the Apache License 2.0'
-author = 'European Nucleotide Archive (ENA).'
+copyright = '2023, European Nucleotide Archive (ENA), Licensed under the Apache License 2.0'
+author = 'European Nucleotide Archive (ENA)'
 
 # makes tabs possible
 extensions = ['sphinx_tabs.tabs']
